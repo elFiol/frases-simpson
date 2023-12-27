9d6ce6055c8011954b5ebb15f2e3afce3c39868a
Fiol Thiago <fiolthiago9@gmail.com>
2023-12-26 21:04:55 -0300

    agregamos el boton y lo centramos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import simpson from './img/The_Simpsons_yellow_logo.svg.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap'
 
 function App() {
-  return <section className="container d-flex justify-content-center">
+  return <section className="container d-flex flex-column justify-content-center">
     <img src={simpson} alt="hola" className='w-50 my-5 mx-auto d-block'/>
+    <Button variant='warning' className='w-75 mx-auto'>Obtener frase</Button>
   </section>
 }
 
