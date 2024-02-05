a6ff2b6ca779e3e4d394b64722214ce253e57c50
Fiol Thiago <fiolthiago9@gmail.com>
2024-02-05 10:45:09 -0300

    cambiamos el button de lugar

--- src/components/SectorMensaje.jsx
+++ src/components/SectorMensaje.jsx
@@ -1,17 +1,23 @@
 import "../style.css";
 import moe from "../img/moe.png";
 import Mensaje from "./Mensaje";
+import { Button } from "react-bootstrap";
 
 function SectorMensaje() {
   return (
-    <article className="container">
-      <h2 className="text-center my-3">
-        <b>Moe</b>
-      </h2>
-      <hr />
-      <img src={moe} alt="moe" className="mx-auto d-block w-25 my-5" />
-      <Mensaje></Mensaje>
-    </article>
+    <>
+      <article className="container">
+        <h2 className="text-center my-3">
+          <b>Moe</b>
+        </h2>
+        <hr />
+        <img src={moe} alt="moe" className="mx-auto d-block w-25 my-5" />
+        <Mensaje></Mensaje>
+      </article>
+      <Button variant="warning" className="w-75 mx-auto my-4">
+        Obtener frase
+      </Button>
+    </>
   );
 }
 
