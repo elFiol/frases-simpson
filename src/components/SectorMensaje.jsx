272907339c3b83326f2e99e08b65f16fb48de595
Fiol Thiago <fiolthiago9@gmail.com>
2024-02-10 22:07:08 -0300

    añadimos la funcion para consultar a la api y ponemos los respectivos nombres e imagenes

--- src/components/SectorMensaje.jsx
+++ src/components/SectorMensaje.jsx
@@ -1,20 +1,26 @@
 import "../style.css";
-import moe from "../img/moe.png";
 import Mensaje from "./Mensaje";
 import { Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 function SectorMensaje() {
+  const [personaje, setPersonaje] = useState({})
+  
+  useEffect(() => {
+    consultarAPI();
+  }, [])
+
+  const consultarAPI = async() => {
+    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
+    const datos = await respuesta.json()
+    setPersonaje(datos[0])
+    console.log(datos)
+  }
+
   return (
     <>
-      <article className="container">
-        <h2 className="text-center my-3">
-          <b>Moe</b>
-        </h2>
-        <hr />
-        <img src={moe} alt="moe" className="mx-auto d-block w-25 my-5" />
-        <Mensaje></Mensaje>
-      </article>
-      <Button variant="warning" className="w-75 mx-auto my-4">
+      <Mensaje personaje={personaje}></Mensaje>
+      <Button variant="warning" onClick={consultarAPI} className="w-75 mx-auto my-4">
         Obtener frase
       </Button>
     </>
